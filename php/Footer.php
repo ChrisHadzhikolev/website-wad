ab8f5395180496644493975ae590d19c274dc549
Kristian Hadzhikolev <kris.hadzhikolev@gmail.com>
2020-04-04 15:39:39 +0200

    Changed Footer, Responsive

--- php/Footer.php
+++ php/Footer.php
@@ -1,34 +1,53 @@
-<footer class="footer">
-    <div class="footer-left">
-        <h3>-> <span>Telepathy Bean</span></h3>
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <h6>-><span>TelepathyBean</span></h6>
+                <p class="text-justify">A recently set-up startup, our goal is to bring fresh solutions to our clients. Right we are offering services for Web Sites and Applications as well as C#/Java Desktop Applications. <br> If you have expertise in Web, Mobile or Desktop Development and want a new challenge feel free to contact us.</p>
+            </div>
 
-        <p class="footer-links">
-            <a href="../index.php">Home</a>
-            |
-            <a href="#">News</a>
-            |
-            <a href="ProjectsPage.php">Projects</a>
-            |
-            <a href="ContactUsPage.php">Contact Us</a>
-            |
-            <a href="AboutUsPage.php">About Us</a>
-            |
-            <a href="MyProfilePage.php">My Profile</a>
-        </p>
+            <div class="col-xs-6 col-md-3">
+                <h6>Support Us</h6>
+                <ul class="footer-links">
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="RYQALGB6WN5KN" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/NL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                        <img alt="" border="0" src="https://www.paypal.com/en_NL/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
 
-        <p class="footer-company-name">© 2020 Telepathy Bean Inc.</p>
+                </ul>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+                <h6>Quick Links</h6>
+                <ul class="footer-links">
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="NewsPage.php">News</a></li>
+                    <li><a href="ProjectsPage.php">Projects</a></li>
+                    <li><a href="ContactUsPage.php">Contact Us</a></li>
+                    <li><a href="AboutUsPage.php">About Us</a></li>
+                    <li><a href="LoginPage.php">Login</a></li>
+                    <li><a href="RegistrationPage.php">Join Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2020 Telepathy Bean BG, Inc. Some Rights Reserved.</p>
+            </div>
 
-    <div class="footer-right">
-        <p class="footer-company-about">
-            <span>About the company</span>
-            Startup. Fresh Solutions. Good Results.</p>
-        <div class="footer-icons">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a href="#"><i class="fa fa-youtube"></i></a>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
