9a58ffaf6f7cc56f73408c070e496b14b3266feb
Kristian Hadzhikolev <kris.hadzhikolev@gmail.com>
2020-04-26 20:30:31 +0200

    registration fix, adding successfully email

--- php/RegistrationPage.php
+++ php/RegistrationPage.php
@@ -23,54 +23,49 @@
 <?php
 require_once "../registration.php";
 ?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <div class="container">
-        <h1>Register</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr>
-<div <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>>
-    <label for="username"><b>Username</b></label>
-    <input type="text" placeholder="Enter username" name="username" required>
-    <span><?php echo $email; ?></span>
-</div>
-        <div<?php echo (!empty($first_name_err)) ? 'has-error' : ''; ?>>
-            <label for="first_name"><b>First name</b></label>
-            <input type="text" placeholder="Enter first name" name="first_name" required>
-            <span><?php echo $first_name_err; ?></span>
+
+<div class="modal">
+    <form class="modal-content animate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="container" >
+            <div <?php echo (!empty($username_err)) ? 'has-error' : '' ; ?>>
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Username" value="<?php echo $username; ?>" name="username" required>
+                <span class="wrong-input"><?php echo $username_err; ?></span>
+            </div>
+            <div<?php echo (!empty($first_name_err)) ? 'has-error' : ''; ?>>
+                <label for="first_name"><b>First name</b></label>
+                <input type="text" placeholder="Enter first name" name="first_name" required>
+                <span class="wrong-input"><?php echo $first_name_err; ?></span>
+            </div>
+            <div <?php echo (!empty($last_name_err)) ? 'has-error' : ''; ?>>
+                <label for="last_name"><b>Last name</b></label>
+                <input type="text" placeholder="Enter last name" name="last_name" required>
+                <span class="wrong-input"><?php echo $last_name_err; ?></span>
+
+            </div>
+            <div <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>>
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter email" name="email" required>
+                <span class="wrong-input"><?php echo $email_err; ?></span>
+            </div>
+
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+                <span class="wrong-input"><?php echo $password_err; ?></span>
+            </div>
+
+            <div <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>>
+                <label for="confirm_password"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" name="confirm_password" required>
+                <span class="wrong-input"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <button type="submit" class="registerbtn">Sign up</button>
         </div>
-        <div <?php echo (!empty($last_name_err)) ? 'has-error' : ''; ?>>
-            <label for="last_name"><b>Last name</b></label>
-            <input type="text" placeholder="Enter last name" name="last_name" required>
-            <span><?php echo $last_name_err; ?></span>
-
+        <div class="container signin">
+            <p>Already have an account? <a href="LoginPage.php">Login</a>.</p>
         </div>
-        <div <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>>
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter email" name="email" required>
-            <span><?php echo $email_err; ?></span>
-        </div>
-
-        <div <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>>
-            <label for="password"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="password" required>
-            <span><?php echo $password_err; ?></span>
-        </div>
-        <div <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>>
-            <label for="confirm_password"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="confirm_password" required>
-            <span><?php echo $confirm_password_err; ?></span>
-        </div>
-
-        <hr>
-        <!--<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>-->
-
-        <button type="submit" class="registerbtn">Sign up</button>
-    </div>
-
-
 </form>
-<div class="container signin">
-    <p>Already have an account? <a href="LoginPage.php">Login</a>.</p>
 </div>
 
 </body>
--- registration.php
+++ registration.php
@@ -1,13 +1,19 @@
 <?php
 require_once "config.php";
 
-$username = $password = $confirm_password = $email = $first_name = $last_name = "";
 $username_err = $password_err = $confirm_password_err = $email_err = $first_name_err = $last_name_err =  "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+    $confirm_password = trim($_POST["confirm_password"]);
+    $email = trim($_POST["email"]);
+    $first_name = trim($_POST["first_name"]);
+    $last_name = trim($_POST["last_name"]);
+
     //Username
-    if(empty(trim($_POST["username"])))
+    if(empty($username))
     {
         $username_err = "Please, enter a username!";
     }
@@ -17,17 +23,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
         if($stmt = $pdo->prepare($sql))
         {
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-
-            $param_username = trim($_POST["username"]);
-
+            $stmt->bindParam(":username", $username, PDO::PARAM_STR);
             if($stmt->execute())
             {
                 if($stmt->rowCount() == 1)
                 {
                     $username_err = "This username is already taken!";
-                } else{
-                    $username = trim($_POST["username"]);
                 }
             }
             else
@@ -37,21 +38,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             unset($stmt);
         }
     }
-
-    if(empty(trim($_POST["first_name"]))) {
+    //First Name
+    if(empty($first_name)) {
     $first_name_err = "Please, enter First Name!";
 }
-    else{
-        $first_name = trim($_POST["first_name"]);
-    }
-    if(empty(trim($_POST["last_name"]))){
+    //Last Name
+    if(empty($last_name)){
         $last_name_err = "Please, enter a Last Name!";
     }
-    else{
-        $last_name = trim($_POST["first_name"]);
-    }
     //Email
-    if(empty(trim($_POST["email"])))
+    if(empty($email))
     {
         $email_err = "Please, enter a valid email!";
     }
@@ -62,16 +58,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             $sql = "SELECT email FROM people_info as pi inner join users as u on pi.username = u.username where email = :email";
 
             if($stmt = $pdo->prepare($sql)){
-                $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
-
-                $param_email = trim($_POST["email"]);
-
+                $stmt->bindParam(":email", $email, PDO::PARAM_STR);
 
                 if($stmt->execute()){
                     if($stmt->rowCount() == 1){
-                        $email_err = "This email is already taken!";
-                    } else{
-                        $email = trim($_POST["email"]);
+                        $email_err = "There is already a user with that email!";
                     }
                 } else{
                     echo "Oops! Something went wrong. Please try again later.";
@@ -81,54 +72,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         }
 
     //Password
-    if(empty(trim($_POST["password"]))){
+    if(empty($password)){
         $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST["password"])) < 8){
+    } else if(strlen($password) < 8){
         $password_err = "Password must be at least 8 characters.";
-    } else{
-        $password = trim($_POST["password"]);
     }
 
-    if(empty(trim($_POST["confirm_password"]))){
+    //Confirm Password
+    if(empty($confirm_password)){
         $confirm_password_err = "Please confirm password.";
     } else{
-        $confirm_password = trim($_POST["confirm_password"]);
         if(empty($password_err) && ($password != $confirm_password)){
             $confirm_password_err = "Password did not match.";
+        }else{
+            $password = password_hash($password, PASSWORD_DEFAULT);
         }
     }
 
-    // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err) && empty($first_name_err)&& empty($last_name_err)){
 
-        // Prepare an insert statement
         $sql = "INSERT INTO users (id, username, password, type_user) VALUES (null, :username, :password, :typeOfUser)";
         $sql1 = "INSERT INTO people_info (id, first_name, last_name, email, username) VALUES (null, :firstName, :lastName, :email, :username)";
 
         if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            $stmt->bindParam(":typeOfUser", $param_userType, PDO::PARAM_STR);
-
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT);
             $param_userType = "Standard";
 
-            // Attempt to execute the prepared statement
+            $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+            $stmt->bindParam(":typeOfUser", $param_userType, PDO::PARAM_STR);
+
             if($stmt->execute()){
                 unset($stmt);
                 if($stmt = $pdo->prepare($sql1)) {
-                    $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-                    $stmt->bindParam(":firstName", $param_first_name, PDO::PARAM_STR);
-                    $stmt->bindParam(":lastName", $param_last_name, PDO::PARAM_STR);
-                    $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
+                    $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+                    $stmt->bindParam(":firstName", $first_name, PDO::PARAM_STR);
+                    $stmt->bindParam(":lastName", $last_name, PDO::PARAM_STR);
+                    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
 
-                    $param_username = $username;
-                    $param_first_name = $first_name;
-                    $param_last_name = $last_name;
-                    $param_email = $email;
                     if($stmt->execute()){
                         header("location: LoginPage.php");
                     }
