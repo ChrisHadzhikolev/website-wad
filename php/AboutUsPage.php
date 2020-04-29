6504bd2ec1ad4192d65d6729a497524c9afa15f1
Kristian Hadzhikolev <kris.hadzhikolev@gmail.com>
2020-04-30 00:10:54 +0200

    new myprofile page

--- php/AboutUsPage.php
+++ php/AboutUsPage.php
@@ -4,7 +4,7 @@
 		<!-- Mobile Specific Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
+		<link rel="shortcut icon" href="../drawable/fav.png">
 		<!-- Author Meta -->
 		<meta name="author" content="colorlib">
 		<!-- Meta Description -->
@@ -21,10 +21,43 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 				<!-- link to the main file for the style of the page-->
 			<link rel="stylesheet" href="../css/aboutUsStyle.css">
-		</head>
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Cookie" type="text/css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Volkhov&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,900,900i">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link
+                rel="stylesheet"
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                crossorigin="anonymous"
+        />
+
+
+        <link rel="stylesheet" href="../css/footerStyle.css">
+        <link rel="stylesheet" href="../css/navigationStyle.css">
+        <link rel="stylesheet" href="../css/backToTopStyle.css">
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script
+                src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+                integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+                crossorigin="anonymous"
+        ></script>
+        <script
+                src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+                integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+                crossorigin="anonymous"
+        ></script>
+        <script
+                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+                integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+                crossorigin="anonymous"
+        ></script>
+
+    </head>
 		<body>	
         <?php
-require_once "php/Navigation.php";
+require_once "Navigation.php";
 ?>
  
 			<section class="home-about-area pt-120">
@@ -43,7 +76,7 @@ require_once "php/Navigation.php";
     What is my occupation?
     I am a first-year-student in Fontys University and a future software engineer and developer.
     Which programming languages I have experience with?
-    I have experienced working with: C#, JavaScript, CSS, HTML, PHP.</p>
+    I have experience working with: C#, JavaScript, CSS, HTML, PHP.</p>
     <a href="#" class="primary-btn text-uppercase">discover now</a>
 						</div>
 					</div>
--- php/MyProfilePage.php
+++ php/MyProfilePage.php
@@ -15,19 +15,38 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Cookie" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Volkhov&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,900,900i">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossorigin="anonymous"
+    />
 
     <link rel="stylesheet" href="../css/myProfileStyle.css">
     <link rel="stylesheet" href="../css/footerStyle.css">
     <link rel="stylesheet" href="../css/navigationStyle.css">
     <link rel="stylesheet" href="../css/backToTopStyle.css">
 
-
-    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Volkhov&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,900,900i" rel="stylesheet">
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"
+    ></script>
+    <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"
+    ></script>
+    <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"
+    ></script>
 
     <title>User Profile</title>
 </head>
@@ -36,16 +55,69 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 require_once "Navigation.php";
 ?>
 
+<div class="container">
+    <h1>My Profile</h1>
+    <hr>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="text-center">
+                <div class="card">
+                    <img class="image" src="../drawable/defPic.png" alt="User">
+                    <h1><?php echo $_SESSION["username"]?></h1>
+                    <p class="title"><?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"] ?></p>
+                    <p><?php echo $_SESSION["type"] . " ";?>User</p>
+                    <p><button onclick="alert('Coming Soon')">Upload Photo</button></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9 personal-info">
+            <h3>Personal info</h3>
+
+            <form class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">First name:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" placeholder="eg. John">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Last name:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" placeholder="eg. Doe">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Email:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" placeholder="eg. john_doe@gmail.com">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Phone Number:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" placeholder="eg. xxxx-xxx-xxx">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-3 control-label"></label>
+                    <div class="col-md-8">
+                        <a href="../edit_info.php">
+                            <input type="button" class="btn btn-primary" value="Edit information">
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<hr>
+
 <a id="back_to_top"></a>
 <script type="text/javascript" src="../js/backToTop.js"></script>
-
-<div class="card">
-    <img src="../drawable/defPic.png" alt="User" style="width:100%">
-    <h1><?php echo $_SESSION["username"]?></h1>
-    <p class="title"><?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"] ?></p>
-    <p><?php echo $_SESSION["type"] . " ";?>User</p>
-    <p><button onclick="alert('Coming Soon')">Contact</button></p>
-</div>
 
 <?php
 require_once "Footer.php";
