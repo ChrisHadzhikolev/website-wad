dda90dfc9e80d1f88ad90c8d66ba5fee1a8ef8df
Jaklin <jakitoo99@gmail.com>
2020-06-11 23:53:43 +0200

    gates and policies

---
+++ blog - Copy/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Access\Response;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class
+    ];
+
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('users_CRUD', function ($user) {
+            return $user->isAdmin;
+        });
+
+        Gate::define('see_messages', function ($user) {
+            return $user->isAdmin;
+        });
+        Gate::define('write_messages', function ($user) {
+            return $user->isUser;
+        });
+
+        Gate::define('users_CRUD', function ($user) {
+            return $user->isAdmin
+                ? Response::allow()
+                : Response::deny('You must be an admin.');
+        });
+        Gate::define('see_messages', function ($user) {
+            return $user->isAdmin
+                ? Response::allow()
+                : Response::deny('You must be an admin to see received messages.');
+        });
+        Gate::define('see_messages', function ($user) {
+            return $user->isAdmin
+                ? Response::allow()
+                : Response::deny('You must be an admin.');
+        });
+    }
+
+}
---
+++ blog - Copy/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
+Route::get('/projects', 'ProjectsController@index');
+Route::get('/contact', 'ContactController@index');
+Route::get('/contact/add', 'IndexController@getAddContact');
+Route::post('/contact/add', 'IndexController@postAddContact');
+Route::get('/contact/{id}', 'IndexController@getContact');
+Route::get('/login', 'LoginController@index');
+Route::get('/register', 'RegisterController@index');
+Route::get('/myProfile', 'MyProfileController@index');
+Route::get('/users', 'UsersController@index');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
